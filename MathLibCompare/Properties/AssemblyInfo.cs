8469def544064793cf0cb5f43cea0908de441ff3
Matteo <matteo.bertola77@gmail.com>
2019-12-30 08:08:24 +0100

    minor changes

--- MathLibCompare/Properties/AssemblyInfo.cs
+++ MathLibCompare/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MathLibTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BLM SPA")]
+[assembly: AssemblyCompany(" ")]
 [assembly: AssemblyProduct("MathLibTest")]
-[assembly: AssemblyCopyright("Copyright © BLM SPA 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
